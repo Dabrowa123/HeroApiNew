51e0f70d4e8e6458a24d37a6a5a29bb5bed8d591
Simon Oakwood <dabrowa.szymon@gmail.com>
2022-12-14 17:38:11 +0100

    removing unecessary import

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./Nav.css";
 import logo from "../../assets/img/nav_logo.png";
 import { Link, useNavigate } from "react-router-dom";
