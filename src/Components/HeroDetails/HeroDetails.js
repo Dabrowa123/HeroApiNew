8ff071af844378f21c4a03b2d7d806cda2d91708
Simon Oakwood <dabrowa.szymon@gmail.com>
2022-12-15 11:19:50 +0100

    adding fade to all card renders

--- src/Components/HeroDetails/HeroDetails.js
+++ src/Components/HeroDetails/HeroDetails.js
@@ -3,6 +3,7 @@ import "./HeroDetails.css";
 import { getAllHeroInfoById } from "../../requests.js";
 import { useParams } from "react-router";
 import Loader from "../Loader/Loader.js";
+import Fade from 'react-reveal/Fade.js';
 import * as icon from "../../assets/icons/index.js";
 import "../Loader/Loader.css";
 
@@ -30,67 +31,69 @@ function HeroDetails() {
         </div>
       )}
       {!isLoading && (
-        <div className="details">
-          <div className="details__info">
-            <img
-              className="details__info__img"
-              src={`${heroData.image?.url}`}
-              alt={`${heroData.name}`}
-            ></img>
-            <div className="details__info__data">
-              <h2>{heroData.name}</h2>
-              <h4>Full name: {heroData.biography?.["full-name"]}</h4>
-              <h4>Eye color: {heroData.appearance?.["eye-color"]}</h4>
-              <h4>Height: {heroData.appearance?.height?.[1]}</h4>
-              <h4>Weight: {heroData.appearance?.weight?.[1]}</h4>
-              <h4>Place of birth: {heroData.biography?.["place-of-birth"]}</h4>
-              <h4>Occupation: {heroData.work?.occupation}</h4>
-              <h4>Aligment: {heroData.biography?.alignment}</h4>
+        <Fade>
+          <div className="details">
+            <div className="details__info">
+              <img
+                className="details__info__img"
+                src={`${heroData.image?.url}`}
+                alt={`${heroData.name}`}
+              ></img>
+              <div className="details__info__data">
+                <h2>{heroData.name}</h2>
+                <h4>Full name: {heroData.biography?.["full-name"]}</h4>
+                <h4>Eye color: {heroData.appearance?.["eye-color"]}</h4>
+                <h4>Height: {heroData.appearance?.height?.[1]}</h4>
+                <h4>Weight: {heroData.appearance?.weight?.[1]}</h4>
+                <h4>Place of birth: {heroData.biography?.["place-of-birth"]}</h4>
+                <h4>Occupation: {heroData.work?.occupation}</h4>
+                <h4>Aligment: {heroData.biography?.alignment}</h4>
+              </div>
+            </div>
+            <div className="details__stats">
+              <div>
+                <img
+                  className="details__stats__icon"
+                  src={icon.combat}
+                  alt="combat icon"
+                />
+                <p>Combat: {heroData.powerstats?.combat}</p>
+              </div>
+              <div>
+                <img
+                  className="details__stats__icon"
+                  src={icon.durability}
+                  alt="durability icon"
+                />
+                <p>Durability: {heroData.powerstats?.durability}</p>
+              </div>
+              <div>
+                <img
+                  className="details__stats__icon"
+                  src={icon.intelligence}
+                  alt="intelligence icon"
+                />
+                <p>Intelligence: {heroData.powerstats?.intelligence}</p>
+              </div>
+              <div>
+                <img
+                  className="details__stats__icon"
+                  src={icon.speed}
+                  alt="combat speed"
+                />
+                <p>Speed: {heroData.powerstats?.speed}</p>
+              </div>
+              <div>
+                <img
+                  className="details__stats__icon"
+                  src={icon.strength}
+                  alt="strength icon"
+                />
+                <p>Strength: {heroData.powerstats?.strength}</p>
+              </div>
             </div>
           </div>
-          <div className="details__stats">
-            <div>
-              <img
-                className="details__stats__icon"
-                src={icon.combat}
-                alt="combat icon"
-              />
-              <p>Combat: {heroData.powerstats?.combat}</p>
-            </div>
-            <div>
-              <img
-                className="details__stats__icon"
-                src={icon.durability}
-                alt="durability icon"
-              />
-              <p>Durability: {heroData.powerstats?.durability}</p>
-            </div>
-            <div>
-              <img
-                className="details__stats__icon"
-                src={icon.intelligence}
-                alt="intelligence icon"
-              />
-              <p>Intelligence: {heroData.powerstats?.intelligence}</p>
-            </div>
-            <div>
-              <img
-                className="details__stats__icon"
-                src={icon.speed}
-                alt="combat speed"
-              />
-              <p>Speed: {heroData.powerstats?.speed}</p>
-            </div>
-            <div>
-              <img
-                className="details__stats__icon"
-                src={icon.strength}
-                alt="strength icon"
-              />
-              <p>Strength: {heroData.powerstats?.strength}</p>
-            </div>
-          </div>
-        </div>
+        </Fade>
       )}
     </section>
   );
--- src/Components/HeroSimplified/HeroSimplified.js
+++ src/Components/HeroSimplified/HeroSimplified.js
@@ -1,58 +1,61 @@
 import React from "react";
+import Fade from 'react-reveal/Fade.js';
 import * as icon from "../../assets/icons/index.js";
 import "./HeroSimplified.css";
 import { Link } from "react-router-dom";
 
 function HeroSimplified({ name, imgUrl, powerstats, id }) {
   return (
-    <div className="featured__hero">
-      <h2>{name}</h2>
-      <Link to={`/hero/${id}`}>
-        <img className="featured__hero__img" src={imgUrl} alt={`${name}`}></img>
-      </Link>
-      <div className="featured__hero__stats">
-        <div>
-          <img
-            className="featured__hero__stats__icon"
-            src={icon.combat}
-            alt="combat icon"
-          />
-          <p>{powerstats.combat}</p>
-        </div>
-        <div>
-          <img
-            className="featured__hero__stats__icon"
-            src={icon.durability}
-            alt="durability icon"
-          />
-          <p>{powerstats.durability}</p>
-        </div>
-        <div>
-          <img
-            className="featured__hero__stats__icon"
-            src={icon.intelligence}
-            alt="intelligence icon"
-          />
-          <p>{powerstats.intelligence}</p>
-        </div>
-        <div>
-          <img
-            className="featured__hero__stats__icon"
-            src={icon.speed}
-            alt="combat speed"
-          />
-          <p>{powerstats.speed}</p>
-        </div>
-        <div>
-          <img
-            className="featured__hero__stats__icon"
-            src={icon.strength}
-            alt="strength icon"
-          />
-          <p>{powerstats.strength}</p>
+    <Fade>
+      <div className="featured__hero">
+        <h2>{name}</h2>
+        <Link to={`/hero/${id}`}>
+          <img className="featured__hero__img" src={imgUrl} alt={`${name}`}></img>
+        </Link>
+        <div className="featured__hero__stats">
+          <div>
+            <img
+              className="featured__hero__stats__icon"
+              src={icon.combat}
+              alt="combat icon"
+            />
+            <p>{powerstats.combat}</p>
+          </div>
+          <div>
+            <img
+              className="featured__hero__stats__icon"
+              src={icon.durability}
+              alt="durability icon"
+            />
+            <p>{powerstats.durability}</p>
+          </div>
+          <div>
+            <img
+              className="featured__hero__stats__icon"
+              src={icon.intelligence}
+              alt="intelligence icon"
+            />
+            <p>{powerstats.intelligence}</p>
+          </div>
+          <div>
+            <img
+              className="featured__hero__stats__icon"
+              src={icon.speed}
+              alt="combat speed"
+            />
+            <p>{powerstats.speed}</p>
+          </div>
+          <div>
+            <img
+              className="featured__hero__stats__icon"
+              src={icon.strength}
+              alt="strength icon"
+            />
+            <p>{powerstats.strength}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </Fade>
   );
 }
 
