24db50b1ed6df50eae792247ff1185268c687b77
Simon Oakwood <dabrowa.szymon@gmail.com>
2022-12-18 13:25:42 +0100

    adding logic to input on landing page

--- src/Components/Searchform/Searchform.js
+++ src/Components/Searchform/Searchform.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./Searchform.css";
 
 function Searchform() {
+  const [searchInputValue, setSearchInputValue] = useState("");
+
+  const navigate = useNavigate();
+  const inputCleanup = () => {
+    document.getElementById("search-input").value = "";
+  };
+
   return (
-      <form className="landing__searchform searchform cf">
-        <input type="text" placeholder="type hero name" />
-        <button type="submit">Find Hero</button>
-      </form>
+    <form className="landing__searchform searchform cf">
+      <input
+        id="search-input"
+        onChange={(event) => setSearchInputValue(event.target.value)}
+        onKeyDown={(event) => {
+          if (event.key === "Enter") {
+            setSearchInputValue(event.target.value);
+            navigate(`/search/${searchInputValue}`);
+            inputCleanup();
+          }
+        }}
+        type="text"
+        name="search"
+        placeholder="type hero name"
+      />
+      <Link to={`/search/${searchInputValue}`}>
+        <button onClick={inputCleanup}>Find Hero</button>
+      </Link>
+    </form>
   );
 }
 
