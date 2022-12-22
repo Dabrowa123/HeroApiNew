f7d109b5c7504ff28237bb6098ceac45fd144928
Simon Oakwood <dabrowa.szymon@gmail.com>
2022-12-22 22:39:01 +0100

    added battleVillains & heroes files

---
+++ src/Components/BattleVillains/BattleVillains.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import { useState, useEffect } from "react";
+// import supermanDesktop from "../../assets/img/superman-desktop.png";
+// import supermanMobile from "../../assets/img/superman-mobile.png";
+// import Searchform from ".././UI/Searchform/Searchform.js";
+// import Fade from "react-reveal/Fade.js";
+// import FeaturedHeroesButton from "../UI/Buttons/FeaturedHeroesButton.js";
+// import PlaceholderImage from "../../assets/img/superman-placeholder.png";
+// import { LazyLoadImage } from "react-lazy-load-image-component";
+// import "react-lazy-load-image-component/src/effects/blur.css";
+
+function BattleVillains() {
+  return <section className="battle-villains"></section>;
+}
+
+export default BattleVillains;
