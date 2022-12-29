976dc784e95339114f0890748f5a47f5977a981f
Simon Oakwood <dabrowa.szymon@gmail.com>
2022-12-29 15:28:46 +0100

    moving hero array to separate file

---
+++ src/Components/Assets/PreloadedData/FeaturedHeroData.js
@@ -0,0 +1,88 @@
+import batmanImg from "../../../assets/img/batman.jpg";
+import freezeImg from "../../../assets/img/freeze.jpg";
+import penguinImg from "../../../assets/img/penguin.jpg";
+
+const featuredHeroesArray = [
+  {
+    id: 70,
+    name: "Batman",
+    imgUrl: batmanImg,
+    powerstats: {
+      combat: "100",
+      durability: "50",
+      id: "70",
+      intelligence: "100",
+      name: "Batman",
+      power: "47",
+      response: "success",
+      speed: "27",
+      strength: "26",
+    },
+  },
+  {
+    id: 457,
+    name: "Mister Freeze",
+    imgUrl: freezeImg,
+    powerstats: {
+      combat: "28",
+      durability: "70",
+      id: "457",
+      intelligence: "75",
+      name: "Mister Freeze",
+      power: "37",
+      response: "success",
+      speed: "12",
+      strength: "32",
+    },
+  },
+  {
+    id: 514,
+    name: "Penguin",
+    imgUrl: penguinImg,
+    powerstats: {
+      combat: "45",
+      durability: "28",
+      id: "514",
+      intelligence: "75",
+      name: "Penguin",
+      power: "30",
+      response: "success",
+      speed: "12",
+      strength: "10",
+    },
+  },
+  {
+    id: 5142,
+    name: "Penguin2",
+    imgUrl: penguinImg,
+    powerstats: {
+      combat: "45",
+      durability: "28",
+      id: "514",
+      intelligence: "75",
+      name: "Penguin",
+      power: "30",
+      response: "success",
+      speed: "12",
+      strength: "10",
+    },
+  },
+  {
+    id: 5143,
+    name: "Penguin3",
+    imgUrl: penguinImg,
+    powerstats: {
+      combat: "45",
+      durability: "28",
+      id: "514",
+      intelligence: "75",
+      name: "Penguin",
+      power: "30",
+      response: "success",
+      speed: "12",
+      strength: "10",
+    },
+  },
+];
+
+export default featuredHeroesArray;
--- src/Components/HeroesFeatured/HeroesFeatured.js
+++ src/Components/HeroesFeatured/HeroesFeatured.js
@@ -1,99 +1,12 @@
 import React, { useRef } from "react";
 import HeroCard from "../HeroCard/HeroCard.js";
-import batmanImg from "../../assets/img/batman.jpg";
-import freezeImg from "../../assets/img/freeze.jpg";
-import penguinImg from "../../assets/img/penguin.jpg";
 import chevronLeft from "../../assets/icons/chevron-left.png";
 import chevronRight from "../../assets/icons/chevron-right.png";
-import "react-multi-carousel/lib/styles.css";
+import featuredHeroesArray from "../Assets/PreloadedData/FeaturedHeroData.js";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
 
 function HeroesFeatured() {
-  const featuredHeroes = [
-    {
-      id: 70,
-      name: "Batman",
-      imgUrl: batmanImg,
-      powerstats: {
-        combat: "100",
-        durability: "50",
-        id: "70",
-        intelligence: "100",
-        name: "Batman",
-        power: "47",
-        response: "success",
-        speed: "27",
-        strength: "26",
-      },
-    },
-    {
-      id: 457,
-      name: "Mister Freeze",
-      imgUrl: freezeImg,
-      powerstats: {
-        combat: "28",
-        durability: "70",
-        id: "457",
-        intelligence: "75",
-        name: "Mister Freeze",
-        power: "37",
-        response: "success",
-        speed: "12",
-        strength: "32",
-      },
-    },
-    {
-      id: 514,
-      name: "Penguin",
-      imgUrl: penguinImg,
-      powerstats: {
-        combat: "45",
-        durability: "28",
-        id: "514",
-        intelligence: "75",
-        name: "Penguin",
-        power: "30",
-        response: "success",
-        speed: "12",
-        strength: "10",
-      },
-    },
-    {
-      id: 5142,
-      name: "Penguin2",
-      imgUrl: penguinImg,
-      powerstats: {
-        combat: "45",
-        durability: "28",
-        id: "514",
-        intelligence: "75",
-        name: "Penguin",
-        power: "30",
-        response: "success",
-        speed: "12",
-        strength: "10",
-      },
-    },
-    {
-      id: 5143,
-      name: "Penguin3",
-      imgUrl: penguinImg,
-      powerstats: {
-        combat: "45",
-        durability: "28",
-        id: "514",
-        intelligence: "75",
-        name: "Penguin",
-        power: "30",
-        response: "success",
-        speed: "12",
-        strength: "10",
-      },
-    },
-  ];
 
   const swiperRef = useRef();
 
@@ -131,7 +44,7 @@ function HeroesFeatured() {
           }}
           className="mySwiper"
         >
-          {featuredHeroes.map(({ name, imgUrl, powerstats, id }) => (
+          {featuredHeroesArray.map(({ name, imgUrl, powerstats, id }) => (
             <SwiperSlide>
               <HeroCard
                 key={id}
