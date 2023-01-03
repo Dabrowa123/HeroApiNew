bb2078a6eb24a04d9a666097aad592bc8ef81458
Simon Oakwood <dabrowa.szymon@gmail.com>
2023-01-03 13:34:46 +0100

    adding useSearch hook to landing page form

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -1,23 +1,23 @@
 import { useNavigate } from "react-router";
 import { useState } from "react";
 
-function useSearch({searchFormId}) {
-    const [searchInputValue, setSearchInputValue] = useState("");
+function useSearch(searchFormId) {
+  const [searchInputValue, setSearchInputValue] = useState("");
 
-    const navigate = useNavigate();
-  
-    const input = document.getElementById({searchFormId});
+  const navigate = useNavigate();
 
-    const handleChange = (event) => {
-      setSearchInputValue(event.target.value);
-    };
-  
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      navigate(`/search/${searchInputValue}`);
-      setSearchInputValue("");
-      input.value = "";
-    };
+  const input = document.getElementById(searchFormId);
+
+  const handleChange = (event) => {
+    setSearchInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    navigate(`/search/${searchInputValue}`);
+    setSearchInputValue("");
+    input.value = "";
+  };
 
   return [handleChange, handleSubmit];
 }
