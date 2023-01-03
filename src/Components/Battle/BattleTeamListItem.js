4b35ab3a35bc394d70ba130942cb0511defa586b
Simon Oakwood <dabrowa.szymon@gmail.com>
2023-01-03 19:25:07 +0100

    removing unecessary data

--- src/Components/Battle/BattleTeamListItem.js
+++ src/Components/Battle/BattleTeamListItem.js
@@ -3,16 +3,6 @@ import * as icon from "../../assets/icons/index.js";
 import BattleRemoveCharacterButton from "../UI/Buttons/BattleRemoveCharacterButton.js";
 
 function BattleTeamListItem({ name, imgUrl, powerstats, id, team }) {
-  // const dispatch = useDispatch();
-
-  // const handleHeroRemoveFromBattle = (id) => {
-  //   if (team === "heroes") {
-  //     dispatch(removeHeroBattle(id));
-  //   } else if (team === "villains") {
-  //     dispatch(removeVillainBattle(id));
-  //   }
-  // };
-
   return (
     <li className="battle-heroes__list-item">
       <img src={imgUrl} width={"50px"} alt={name} />
@@ -20,12 +10,6 @@ function BattleTeamListItem({ name, imgUrl, powerstats, id, team }) {
         <div className="battle-heroes__list-item-info-details">
           {name}
           <BattleRemoveCharacterButton id={id} team={team} />
-          {/* <button
-            className="battle-heroes__remove-button"
-            onClick={() => handleHeroRemoveFromBattle(id)}
-          >
-            Remove
-          </button> */}
         </div>
         <div className="battle-heroes__list-item-info-details">
           <div className="battle-heroes__list-item-stats">
