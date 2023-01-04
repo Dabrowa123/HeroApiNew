9baa31ca39ccbdc9165538f8bd4082d86b256545
Simon Oakwood <dabrowa.szymon@gmail.com>
2023-01-04 10:14:29 +0100

    reducing battle hero card & hero card to one

--- src/Components/HeroCard/HeroCard.js
+++ src/Components/HeroCard/HeroCard.js
@@ -4,12 +4,42 @@ import { Link } from "react-router-dom";
 import PlaceholderImage from "../../assets/img/400x500_placeholder.jpg";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
+import { useDispatch } from "react-redux";
+import { addHeroBattle, addVillainBattle } from "../../store/index.js";
 import renderPowerstats from "../../helpers/renderPowerstats.js";
+import useBattleMode from "../../hooks/useBattleMode.js";
 
 function HeroCard({ name, imgUrl, powerstats, id }) {
+  const [battleMode] = useBattleMode();
+  const dispatch = useDispatch();
+  const handleAddToHeroes = () => {
+    const hero = { name, imgUrl, powerstats, id };
+    dispatch(addHeroBattle(hero));
+  };
+  const handleAddToVillains = () => {
+    const hero = { name, imgUrl, powerstats, id };
+    dispatch(addVillainBattle(hero));
+  };
+
   return (
     <Fade>
       <div className="hero-card">
+      {battleMode && (
+        <div className="battle-hero-card__buttons-container">
+          <span
+            className="battle-hero-card__btn battle-hero-card__btn--add"
+            onClick={handleAddToHeroes}
+          >
+            Add to Heroes
+          </span>
+          <span
+            className="battle-hero-card__btn battle-hero-card__btn--remove"
+            onClick={handleAddToVillains}
+          >
+            Add to Villains
+          </span>
+        </div>
+      )}
         <h2 className="hero-card__heading">{name}</h2>
         <div className="hero-card__img-container">
           <Link to={`/hero/${id}`}>
