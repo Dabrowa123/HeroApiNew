f58a156ad90eead25560043baa624c5eeceeb785
Simon Oakwood <dabrowa.szymon@gmail.com>
2023-01-05 13:52:31 +0100

    refactor useSearchResults hook and usage in battle search view

--- src/hooks/useSearchResults.js
+++ src/hooks/useSearchResults.js
@@ -1,20 +1,27 @@
 import { useEffect, useState } from "react";
 import { searchHeroesByName } from "../requests.js";
+import useBattleMode from "./useBattleMode.js";
 
 function useSearchResults(name) {
   const [searchList, setSearchListContent] = useState([]);
-  const [characterNotFound, setcharacterNotFound] = useState(false);
+  const [characterNotFound, setCharacterNotFound] = useState(false);
   const [isLoading, setLoadingState] = useState(true);
+  const [pageInitState, setPageInitState] = useState(true);
+  const [battleMode] = useBattleMode();
   const uppercaseName = name.charAt(0).toUpperCase() + name.slice(1);
 
   useEffect(() => {
+    if (name === "") {
+      return;
+    }
+    setPageInitState(false);
     setLoadingState(true);
     searchHeroesByName(name).then((searchResults) => {
       const { data } = searchResults;
 
       if (data.error) {
         setSearchListContent([]);
-        setcharacterNotFound(true);
+        setCharacterNotFound(true);
         setLoadingState(false);
         return;
       }
@@ -22,11 +29,30 @@ function useSearchResults(name) {
       const { results } = data;
 
       setSearchListContent(results);
+
+      if (battleMode) {
+        // Do not render heroes with empty powerstats
+        const validatedResults = results.flatMap((hero) =>
+          Object.values(hero.powerstats).includes("null", 0) ? [] : hero
+        );
+        setSearchListContent(validatedResults);
+      } else {
+        setSearchListContent(results);
+      }
+
+      setPageInitState(false);
+      setCharacterNotFound(false);
       setLoadingState(false);
     });
   }, [name]);
 
-  return [searchList, characterNotFound, isLoading, uppercaseName];
+  return [
+    searchList,
+    characterNotFound,
+    isLoading,
+    uppercaseName,
+    pageInitState,
+  ];
 }
 
 export default useSearchResults;
